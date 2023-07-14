863be7e70968a48c765c70d2a93357cdf665da1a
うなぎてりやき <118857786+Konbini98@users.noreply.github.com>
2023-07-15 01:51:56 +0630

    Update database.py
    Signed-off-by: うなぎてりやき <118857786+Konbini98@users.noreply.github.com>

--- bot/helpers/database.py
+++ bot/helpers/database.py
@@ -94,7 +94,7 @@ class DatabaseHelper:
     async def get_all_users(self):
         if self.__err:
             return
-        all_users = self.__col.find({"users_id"})
+        all_users = self.__col.find({"id"})
         self.__client.close()
         return all_users
 
